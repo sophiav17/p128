0f5ec601e80aff9de28caecc668668fdeca3bcda
Sophia <99284300+sophiav17@users.noreply.github.com>
2023-01-31 17:47:59 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+hp_theme_song = "";
+pp_song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+function preload()
+{
+   hp_theme_song = loadSound("harry_potter_theme_1.mp3");
+   pp_song = loadSound("peter_pans_flight.mp3");
+}
+
+function setup()
+{
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log("PoseNet Is Initialized");
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristX = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+    }
+}
+
+function draw()
+{
+    image(video, 0, 0, 600, 500);
+}
